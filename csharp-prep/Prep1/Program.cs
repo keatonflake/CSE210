2400dea5ee02ce6b1aa127dc6a676836980af091
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2023-04-25 14:29:27 -0600

    practice

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First Name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your Last Name? ");
+        string lastName = Console.ReadLine();
+        
+        Console.Write($"Your Name is {lastName}, {firstName} {lastName}");
+
     }
 }
