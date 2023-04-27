9baa8d02cff0148dda54f7dc6a887c05e752d8f1
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2023-04-27 14:16:33 -0600

    created Magic number guessing game

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;
+
+        Console.WriteLine("Hello! Type your grade percentage... ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        if (grade >= A){
+        Console.WriteLine("you got an A!! ");
+        }
+        else if (grade >= B){
+        Console.WriteLine("you got an B! ");
+        }
+        else if (grade >= C){
+        Console.WriteLine("you got an C! ");
+        }
+        else if (grade >= D){
+        Console.WriteLine("you got an D! ");
+        }
+        else{
+        Console.WriteLine("you got an F! ");
+        }
+
+        if (grade >= C){
+        Console.WriteLine("you passed! ");
+        }
+        else{
+        Console.WriteLine("you can do better! ");
+        }
+
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("state the magic number: ");
+        // string input = Console.ReadLine();
+        // int magicNumber = int.Parse(input);
+        Random generator = new Random();
+        int magicNumber = generator.Next(1,101);
+        Console.WriteLine("Now guess the Magic number: ");
+        string guessInput = Console.ReadLine();
+        int guess = int.Parse(guessInput);
+
+        while (magicNumber != guess){
+            if (magicNumber < guess){
+                Console.WriteLine("Guess Lower! ");
+                Console.WriteLine("Guess the Magic number: ");
+                guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+            }
+            else{
+                Console.WriteLine("Guess Higher: ");
+                Console.WriteLine("Now guess the Magic number: ");
+                guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);
+            }
+        }
+        
+        if (magicNumber == guess) {
+            Console.WriteLine("Nailed it! ");
+        }
     }
 }
