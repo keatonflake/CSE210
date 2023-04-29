35186ba3bf1b6ddaa056b7a4fecc125610a8490b
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2023-04-29 11:58:06 -0600

    prep 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("iTS wOrKiNG!!! ");
+        // Compute the sum, or total, of the numbers in the list.
+        // Compute the average of the numbers in the list.
+        // Find the maximum, or largest, number in the list.
+        bool repeat = true;
+        var numbers = new List<int>();
+        int sum = 0;
+        float average = 0;
+        int count = 0;
+        int biggest = 0;
+        int smallest = 1000000;
+        while (repeat){
+            Console.WriteLine("Type Numbers to add to the list. When done enter the number 0");
+            int newNumber = int.Parse(Console.ReadLine());
+            numbers.Add(newNumber);
+
+            if (newNumber == 0){
+                repeat = false;
+            }
+        }
+        foreach (int number in numbers){
+            sum += number;
+            count += 1;
+            average = sum / count;
+            if (number > biggest){
+                biggest = number;
+            } 
+            if (number != 0){
+                if (smallest > number){
+                    smallest = number;
+                }
+            }
+        }
+        Console.WriteLine(sum);
+        Console.WriteLine(average);
+        Console.WriteLine(biggest);
+        Console.WriteLine(smallest);
+
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+    // DisplayWelcome - Displays the message, "Welcome to the Program!"
+    // PromptUserName - Asks for and returns the user's name (as a string)
+    // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+    // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    // DisplayResult - Accepts the user's name and the squared number and displays them.
+    DisplayWelcome();
+    
+    string name = PromptUserName();
+    int number = PromptUserNumber();
+    int squared = SquareNumber(number);
+    
+    DisplayResult(name,squared);
+    }
+    
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program! ");
+    }
+    static string PromptUserName(){
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hey {name} whats up dawg ");
+        return name;
+    }
+    static int PromptUserNumber(){
+        Console.WriteLine("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber(int number){
+       int squared = number * number;
+       return squared;
+    }
+    static void DisplayResult(string name, int squared){
+        Console.WriteLine($"{name}'s favorite number squared is {squared}");
     }
 }
