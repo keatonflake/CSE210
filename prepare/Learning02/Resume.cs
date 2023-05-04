bc34d04a1ce2c3a5f84d656b9446d8d4d2dfcd05
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2023-05-04 12:00:23 -0600

    added for loop

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -2,6 +2,12 @@ public class Resume{
     public string _name;
     public List<string> _jobs = new List<string>();
     public void Display(){
-        Console.WriteLine($"{_name} {_jobs[0]}");
+        Console.WriteLine($"{_name}");
+
+        foreach (string _job in _jobs){
+            Console.WriteLine(_job);
+
+        }
+        // Console.WriteLine($"{_name} {_jobs[0]}");
     }
 }
